21c3208d4027b389e381ed838ba61cbf55e6d8e2
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-10-06 19:21:49 +0700

    add icon in showItem.tsx

--- src/features/todo/todo-components/ShowTodo.tsx
+++ src/features/todo/todo-components/ShowTodo.tsx
@@ -2,7 +2,7 @@ import type { JSX } from "react"
 import { TodoItem } from "./TodoItem"
 import { TodoSliceState } from "../todoSlice"
 export const ShowTodo = ({todos} : {todos : TodoSliceState[]  }): JSX.Element => {
-  return <div>
+  return <div className=" w-full">
     {
       todos.map(todo=> <TodoItem key={todo.id} todo={todo} />)
     }
--- src/features/todo/todo-components/TodoItem.tsx
+++ src/features/todo/todo-components/TodoItem.tsx
@@ -1,5 +1,12 @@
 import type { JSX } from "react"
+
+import { FaRegCircle } from "react-icons/fa6";
+import { FaTrashCan } from "react-icons/fa6";
 import { TodoSliceState } from "../todoSlice"
-export const TodoItem =({todo} :{todo : TodoSliceState}): JSX.Element => {
-    return <div>{todo.title}</div>
+export const TodoItem = ({ todo }: { todo: TodoSliceState }): JSX.Element => {
+  return <div className="flex justify-center items-center gap-3 my-2">
+    <FaRegCircle className="mr-2 text-green-700" />
+    {todo.title}
+    <FaTrashCan className="ml-auto text-red-500" />
+  </div>
 }
