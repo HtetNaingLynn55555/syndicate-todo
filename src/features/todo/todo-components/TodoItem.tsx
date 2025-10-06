9d8d16fcb12b46ed78de8b97870c78ff2dca45df
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-10-07 05:05:58 +0700

    add details icon

--- src/features/todo/todo-components/TodoItem.tsx
+++ src/features/todo/todo-components/TodoItem.tsx
@@ -1,36 +1,34 @@
 import type { JSX } from "react"
 import { useState } from "react"
 import { FaTrashCan } from "react-icons/fa6"
+import { FaEye } from "react-icons/fa6"
 import { TodoSliceState } from "../todoSlice"
 export const TodoItem = ({ todo }: { todo: TodoSliceState }): JSX.Element => {
+  const [isCompleted, setIsCompleted] = useState(todo.completed)
 
-    const [isCompleted, setIsCompleted] = useState(todo.completed);
-
-    const onRadioHandler = () => {
-        setIsCompleted(!isCompleted);
-    }
+  const onRadioHandler = () => {
+    setIsCompleted(!isCompleted)
+  }
   return (
     <div className="grid grid-cols-8 my-2 ">
-        
-        <div className="flex  items-center col-start-1 col-end-2">
-         
-          <input type="checkbox"
-          
-            checked={isCompleted}
-            onChange={onRadioHandler}
-           className="w-3 h-3 accent-green-500
-            border border-gray-400 rounded-full cursor-pointer"/>
-        </div>
-    
+      <div className="flex  items-center col-start-1 col-end-2">
+        <input
+          type="checkbox"
+          checked={isCompleted}
+          onChange={onRadioHandler}
+          className="w-3 h-3 accent-green-500
+            border border-gray-400 rounded-full cursor-pointer"
+        />
+      </div>
 
       <div className="col-start-2 col-end-7">
-        <div className="flex items-center"
-        >{todo.title}</div>
+        <div className="flex items-center">{todo.title}</div>
       </div>
-      <div className="col-start-7 col-end-8 flex  items-center">
-<FaTrashCan className="ml-auto  hover:cursor-pointer text-red-500" />
+      <div className="col-start-7 col-end-8 flex sm:gap-4 items-center justify-end">
+        <FaEye className="hover:cursor-pointer text-yellow-400" />
+
+        <FaTrashCan className="  hover:cursor-pointer text-red-700" />
       </div>
-      
     </div>
   )
 }
