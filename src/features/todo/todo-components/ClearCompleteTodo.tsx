f77b1b0de424695379f19d65d0a57cd9d72acff1
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-10-06 22:35:35 +0700

    style change for clear complete and todofilter

--- src/features/todo/todo-components/ClearCompleteTodo.tsx
+++ src/features/todo/todo-components/ClearCompleteTodo.tsx
@@ -2,12 +2,10 @@ import { JSX } from "react"
 
 export const ClearCompleteTodo = (): JSX.Element => {
   return (
-    <div className="col-start-1 flex justify-end col-end-13 sm:col-start-7 sm:col-end-9 ">
-      
-     <div className="underline underline-offset-2 hover:decoration-teal-500 hover:text-teal-500 cursor-pointer ">
+    <div className="col-start-1 flex sm:justify-end col-end-13 sm:col-start-7 sm:col-end-9 ">
+      <div className="hover:underline underline-offset-2 hover:decoration-teal-500 hover:text-teal-500 cursor-pointer ">
         Clear completed
-     </div>
-     
+      </div>
     </div>
   )
 }
--- src/features/todo/todo-components/TodoFilter.tsx
+++ src/features/todo/todo-components/TodoFilter.tsx
@@ -1,5 +1,11 @@
 import type { JSX } from "react"
 
 export const TodoFilter = (): JSX.Element => {
-  return <div className=" col-start-1 col-end-13 sm:col-start-3 sm:col-end-7 ">Todo Filter</div>
+  return (
+    <div className=" col-start-1 col-end-13 sm:col-start-3 sm:col-end-7 flex sm:justify-around justify-between items-start ">
+      <div>All</div>
+      <div>Active</div>
+      <div>Completed</div>
+    </div>
+  )
 }
